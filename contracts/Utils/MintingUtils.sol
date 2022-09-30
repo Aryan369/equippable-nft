ad4425a5d687e73cf534603e011ea475cc634f1c
Aryan-369 <71432096+Aryan369@users.noreply.github.com>
2022-09-30 21:45:32 +0530

    /

--- contracts/Utils/MintingUtils.sol
+++ contracts/Utils/MintingUtils.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-
-pragma solidity ^0.8.15;
 
 contract MintingUtils is Ownable {
     using Counters for Counters.Counter;
